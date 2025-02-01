20075c0f6c2b154f6fc81a81bf77b18993c8b061
MRPERFECT0603 <vivekshaurya62@gmail.com>
2025-02-02 00:33:51 +0530

    feat: configured the redis cloud database for caching.

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express, { Express} from "express";
+import express, { Express } from "express";
 import { connectDB } from "./Config/dbConfig";
 import { connectRedis } from "./Config/redisConfig";
 import dotenv from "dotenv";
@@ -7,14 +7,14 @@ import faqRouter from "./Routes/faqRoutes";
 const PORT = process.env.PORT || 8000;
 dotenv.config();
 
-const app:Express = express();
+const app: Express = express();
 connectDB();
 connectRedis();
 //middleware
 app.use(express.json());
-app.use("/api" , faqRouter);
+app.use("/api", faqRouter);
 
 
-app.listen(PORT , ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
